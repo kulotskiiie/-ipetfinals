a33a8cc3ca6446ef62f6899fe6c82a5f7487cc04
Kristel Bautista <rosekylaf@gmail.com>
2021-03-06 18:45:41 +0800

    integ

--- config.php
+++ config.php
@@ -2,12 +2,12 @@
 
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', 'test');
+    define('DB_PASSWORD', '');
     define('DB_NAME', 'test');
 
     $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-    if ($link === false) {
+    if ($link === true) {
         die('Error: Could not connect . ' . mysqli_connect_error());
     }
 
